163e619ffd7c610a63d874e3ddf0fc4043fa2d23
helciodev <helcioandre@live.com>
2020-12-10 05:50:51 +0100

    adds method ask_user

--- lib/scraper_logic.rb
+++ lib/scraper_logic.rb
@@ -17,10 +17,28 @@ class Scraper
         $countries_stats.each do |country|
           print_country(country)
         end
-        
+    end
+
     def filter_country(countries, name)
         name = name
         country = countries.select { |nation| nation[:name].upcase == name.upcase }
        country[0]
-      end
+    end
+
+  def ask_user
+    
+    prompt = TTY::Prompt.new
+    puts ""
+    user_input = prompt.select("Choose 'All' or a specific country to know stats     about covid-19?", 'All', $countries_stats.each { |country_name| country_name    [:name] }).capitalize
+            
+    case user_input
+      when 'All'
+      print_all
+      else
+      print_country(filter_country($countries_stats, user_input))
+    end
+    font = TTY::Font.new(:doom)
+    puts font.write("thanks for using").light_blue
+  end
+
 end
